31b70d623544f147f7dd03629ee01d5db8d36d24
upasana <upasanagaur55@gmail.com>
2025-07-10 15:34:15 +0530

    day4

--- lec-3/lec-3continue/index.js
+++
@@ -1,2 +0,0 @@
-const mypackage = require("@upasana.25/myfirstpackage");
-console.log(mypackage.sum(2, 3)); // 5
---
+++ lec-3/lec3-continued/index.js
@@ -0,0 +1,3 @@
+let mypackage=require("@upasana.25/spackage");
+console.log(mypackage.sum(2,3));
+// console.log("mypackage:", mypackage);
