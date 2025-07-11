93e709f75e2b731ac3be7402d310fa7916deb78d
upasana2515 <156989249+upasana2515@users.noreply.github.com>
2025-07-11 14:18:22 +0530

    Update index.js

--- lec-3/index.js
+++ lec-3/index.js
@@ -1,7 +1,4 @@
 const express = require("express");
 // console.log(express);
-<<<<<<< HEAD
+
 module.exports = require("./lib/sum");
-=======
-module.exports = require("./lib/sum");
->>>>>>> ad2a5f8d2f1a1cf1270eee9763b066e3841e9e66
